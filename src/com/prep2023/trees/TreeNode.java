31b1359ff6b1bf4bef8a96bf65e302ed4fba8da6
Bhupendra Shekhawat <bhupendrashekhawat1108@gmail.com>
2023-10-23 07:42:38 +0530

    Add more tree problems and some stack/queue

---
+++ src/com/prep2023/trees/TreeNode.java
@@ -0,0 +1,10 @@
+package com.prep2023.trees;
+
+class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
